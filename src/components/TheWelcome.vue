8aa1a9976f3f61f0de8cac74efeb03fc73f0fee6
武林萌主 <49557141+Aurora2233@users.noreply.github.com>
2022-10-14 22:00:46 +0800

    init

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,28 @@
+<template>
+  <a-input-search
+    v-model:value="value.search"
+    placeholder="input search domian url"
+    enter-button
+    @search="onSearch"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from 'vue';
+export default defineComponent({
+  setup() {
+    const value = reactive({
+      search: 'https://www.xbiquge.so/book/54523/',
+    });
+
+    const onSearch = () => {
+      console.log('or use this.value', value);
+    };
+
+    return {
+      value,
+      onSearch,
+    };
+  },
+});
+</script>
